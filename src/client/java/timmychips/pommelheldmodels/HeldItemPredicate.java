1aa54f0717d8cb22231be1f16e1e527a8c30c37c
TimmyChips <jhinners@outlook.com>
2025-02-21 18:42:19 -0500

    1.20.1 - Added new render types resource packs can use

--- src/client/java/timmychips/pommelheldmodels/HeldItemPredicate.java
+++ src/client/java/timmychips/pommelheldmodels/HeldItemPredicate.java
@@ -3,25 +3,48 @@ package timmychips.pommelheldmodels;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.util.Identifier;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Arrays;
 
 public class HeldItemPredicate {
     public static ModelTransformationMode currentItemRenderMode;
 
+    // 
+    private static final String namespace = "pommel";
+    private static final String render_held = "is_held";
+    private static final String render_fixed = "is_fixed";
+    private static final String render_ground = "is_ground";
+
+    // 
+    private static HashMap<Identifier, List<ModelTransformationMode>> renderTypeWhitelist;
+
     public static void registerHeldModelPredicate() {
-        // Registers "pommel:is_held" item predicate for use in item models
-        ModelPredicateProviderRegistry.register(Identifier.of("pommel", "is_held"), (itemStack, world, livingEntity, i) -> {
 
-            // Returns is_held item predicate to 0.0F, so it'll render the standard item model
-            if (currentItemRenderMode == null) {
-                return 0.0F;
-            }
+        // Creates association to render type and transformation modes
+        // HashMap contains Indentifiers (held, on ground) with several mode types linked to each identifier
+        renderTypeWhitelist = new HashMap() {{
+            put(Identifier.of(namespace, render_held), Arrays.asList( // Held render modes
+                    ModelTransformationMode.FIRST_PERSON_LEFT_HAND,
+                    ModelTransformationMode.FIRST_PERSON_RIGHT_HAND,
+                    ModelTransformationMode.THIRD_PERSON_LEFT_HAND,
+                    ModelTransformationMode.THIRD_PERSON_RIGHT_HAND));
 
-            // Returns is_held item predicate for model
-                // 0.0F is rendered when in GUI, on ground, or fixed (in item frame)
-                // 1.0F is rendered when it's held in hand
-            return currentItemRenderMode == ModelTransformationMode.GUI
-                    || currentItemRenderMode == ModelTransformationMode.GROUND
-                    || currentItemRenderMode == ModelTransformationMode.FIXED ? 0.0F : 1.0F;
-        });
+            put(Identifier.of(namespace, render_fixed), Arrays.asList( // Item Frame, Fixed render mode
+                    ModelTransformationMode.FIXED));
+
+            put(Identifier.of(namespace, render_ground), Arrays.asList( // Thrown on ground render mod
+                    ModelTransformationMode.GROUND));
+        }};
+
+        for (var entry:renderTypeWhitelist.entrySet()) { // Performs for each key-value pair
+                                                         // Performs for each Identifier and associated List items
+
+            ModelPredicateProviderRegistry.register(entry.getKey(), (itemStack, world, livingEntity, i) -> { // Registers Identifier key
+                // If Null or not whitelisted, return 0 or else return 1
+                return currentItemRenderMode == null
+                        || !entry.getValue().contains(currentItemRenderMode) ? 0.0F : 1.0F;
+            });
+        }
     }
 }
