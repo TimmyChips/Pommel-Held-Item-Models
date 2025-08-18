bce9140c2d2dafe6ffbebb250279546f673c0991
TimmyChips <jhinners@outlook.com>
2025-08-18 16:05:20 -0500

    HeldItemPredicate.java cleanup wip and first_thirdperson predicate

--- src/client/java/timmychips/pommelheldmodels/HeldItemPredicate.java
+++ src/client/java/timmychips/pommelheldmodels/HeldItemPredicate.java
@@ -20,7 +20,7 @@ public class HeldItemPredicate {
 
     private static final String namespace = "pommel";
     private static final String render_held = "is_held";
-    private static final String render_first_thirdperson = "first_third_person"; // 0F means first person, 1F means third person
+    private static final String render_first_thirdperson = "first_third_person"; // 0.5F means first person, 1F means third person
     private static final String render_offhand = "is_offhand";
     private static final String render_fixed = "is_fixed";
     private static final String render_ground = "is_ground";
@@ -36,12 +36,7 @@ public class HeldItemPredicate {
             ModelTransformationMode.THIRD_PERSON_RIGHT_HAND
     );
 
-    private static final List<ModelTransformationMode> renderModeFirst = Arrays.asList(
-            ModelTransformationMode.FIRST_PERSON_LEFT_HAND,
-            ModelTransformationMode.FIRST_PERSON_RIGHT_HAND
-    );
-
-    private static final List<ModelTransformationMode> renderModeThird = Arrays.asList(
+    private static final List<ModelTransformationMode> renderModeThird = Arrays.asList( // To check if current render mode is third person
             ModelTransformationMode.THIRD_PERSON_LEFT_HAND,
             ModelTransformationMode.THIRD_PERSON_RIGHT_HAND
     );
@@ -68,6 +63,7 @@ public class HeldItemPredicate {
         predicateMap.addToMap(render_fixed, ModelTransformationMode.FIXED);
         predicateMap.addToMap(render_ground, ModelTransformationMode.GROUND);
         predicateMap.addToMap(render_head, ModelTransformationMode.HEAD);
+        predicateMap.addToMap(render_first_thirdperson, renderModeHands);
     }
 
     public static void registerHeldModelPredicate() {
@@ -84,14 +80,30 @@ public class HeldItemPredicate {
                 boolean isSubmergedPredicate = entry.getKey().getPath().equals(render_submerged);
                 boolean isGroundPredicate = entry.getKey().getPath().equals(render_ground);
                 boolean isThrownPredicate = entry.getKey().getPath().equals(render_thrown);
+                boolean isFirstThirdPredicate = entry.getKey().getPath().equals(render_first_thirdperson);
+
+                String predicate = entry.getKey().getPath();
 
                 if (livingEntity != null) {
                     // Predicate when player presses the use key for the using item predicate + item is in hand
-                    if (isUsedPredicate && matchesItemInHand(livingEntity, itemStack)) return UseKeyTracker.playerUseItemKey(livingEntity, itemStack);
+                    if (predicate.equals(render_using) && matchesItemInHand(livingEntity, itemStack)) return UseKeyTracker.playerUseItemKey(livingEntity, itemStack);
 
                     // Predicate when player is in water
-                    if (isSubmergedPredicate) return livingEntity.isSubmergedInWater() ? 1.0F : 0.0F;
+                    if (predicate.equals(render_submerged)) return livingEntity.isSubmergedInWater() ? 1.0F : 0.0F;
                 }
+
+                if (currentItemRenderMode == null) return 0.0F; // Return 0 if render mode is null
+                // Do this after those other predicates so that those can render in the gui
+
+                return switch (predicate) {
+                    case render_offhand -> itemInOffhand && entry.getValue().contains(currentItemRenderMode) ? 1.0F : 0.0F;
+                    case render_first_thirdperson -> firstThirdPersonCheck();
+                    case render_thrown -> isFlyingItem && entry.getValue().contains(currentItemRenderMode) ? 1.0F : 0.0F;
+                    case render_ground -> !isFlyingItem && entry.getValue().contains(currentItemRenderMode) ? 1.0F : 0.0F;
+                    default -> entry.getValue().contains(currentItemRenderMode) ? 1.0F : 0.0F;
+                };
+
+                /*
 
                 if (currentItemRenderMode == null) return 0.0F; // Return 0 if render mode is null
                 // Do this after those other predicates so that those can render in the gui
@@ -99,6 +111,13 @@ public class HeldItemPredicate {
                 // If in offhand, return 1 for the offhand predicate
                 // Note that this makes is_held and is_offhand both return 1
                 if (isOffhandPredicate) return (itemInOffhand && entry.getValue().contains(currentItemRenderMode)) ? 1.0F : 0.0F;
+
+                // Return float based for first_thirdperson predicate if render mode is first or third person
+                if (isFirstThirdPredicate) {
+                    if (currentItemRenderMode.isFirstPerson()) return 0.5F;
+                    else if (renderModeThird.contains(currentItemRenderMode)) return 1F;
+                    else return 0F;
+                }
 
                 // For flying/thrown items
                 if (isThrownPredicate) return isFlyingItem && entry.getValue().contains(currentItemRenderMode) ? 1.0F : 0.0F;
@@ -108,6 +127,8 @@ public class HeldItemPredicate {
                 // Return 1 if whitelisted for all other predicates
                 if (!isUsedPredicate) return entry.getValue().contains(currentItemRenderMode) ? 1.0F : 0.0F;
                 else return 0.0F;
+
+                 */
             });
         }
     }
@@ -115,5 +136,11 @@ public class HeldItemPredicate {
     public static boolean matchesItemInHand(LivingEntity entity, ItemStack stack) {
         ItemStack currentItem = entity.getMainHandStack().isEmpty() ? entity.getOffHandStack() : entity.getMainHandStack();
         return stack.toString().equals(currentItem.toString());
+    }
+
+    private static float firstThirdPersonCheck() {
+        if (currentItemRenderMode.isFirstPerson()) return 0.5F;
+        else if (renderModeThird.contains(currentItemRenderMode)) return 1F;
+        else return 0F;
     }
 }
