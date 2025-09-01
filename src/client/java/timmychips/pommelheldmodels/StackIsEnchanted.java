16f98cfecec680a88ee3ed9e30abfce44c8842bd
TimmyChips <jhinners@outlook.com>
2025-08-31 23:39:32 -0500

    StackIsEnchanted check converted to nbt format

--- src/client/java/timmychips/pommelheldmodels/StackIsEnchanted.java
+++ src/client/java/timmychips/pommelheldmodels/StackIsEnchanted.java
@@ -1,34 +1,17 @@
 package timmychips.pommelheldmodels;
 
-import net.minecraft.component.ComponentChanges;
-import net.minecraft.component.ComponentType;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 
 /**
  * Returns 1 if ItemStack is enchanted and 0 if not
  */
 public class StackIsEnchanted {
     static float getValue(ItemStack stack) {
-        ComponentType<ItemEnchantmentsComponent> enchantments = DataComponentTypes.ENCHANTMENTS;
-        ComponentType<ItemEnchantmentsComponent> storedEnchantments = DataComponentTypes.STORED_ENCHANTMENTS;
-
-        boolean hasStoredEnchantments = stack.contains(storedEnchantments);
-
-        // Checks if stack enchantment component has changed from default, or if enchanted book has stored enchantment component
-        if (stack.contains(enchantments) || hasStoredEnchantments) {
-            return componentHasChanged(enchantments, stack) ||
-                    hasStoredEnchantments ? 1F : 0F;
+        NbtCompound nbt = stack.getNbt();
+        if (nbt != null) {
+            return nbt.contains("Enchantments") || nbt.contains("StoredEnchantments") ? 1F : 0F;
         }
         else return 0F;
-    }
-
-    // Needed since all items technically have enchantment component, so need to check if component has changed
-    private static boolean componentHasChanged(ComponentType<?> componentType, ItemStack stack) {
-        ComponentChanges componentChanges = stack.getComponentChanges();
-
-        return componentChanges.entrySet().stream()
-                .anyMatch(entry -> entry.getKey().equals(componentType));
     }
 }
